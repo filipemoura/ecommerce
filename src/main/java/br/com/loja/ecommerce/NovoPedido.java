7ccfca57f8fef1202293f484a9c272511aa2cf0b
Filipe Alves <filipemouraalves@gmail.com>
2022-03-29 15:13:01 -0300

    Refatorando producers e consumers

--- src/main/java/br/com/loja/ecommerce/NovoPedido.java
+++ src/main/java/br/com/loja/ecommerce/NovoPedido.java
@@ -1,45 +1,21 @@
 package br.com.loja.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NovoPedido {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 5; i++) {
+                var key = UUID.randomUUID().toString();
 
-        var value = "1, 2, 3";
-        var record = new ProducerRecord<>("ECOMMERCE_NOVO_PEDIDO", value, value);
+                var value = key + ", 2, 3";
+                dispatcher.send("ECOMMERCE_NOVO_PEDIDO", key, value);
 
-        var email = "Obrigado por seu pedido! Nós estamos processando!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_ENVIAR_EMAIL", email, email);
-
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+                var email = "Obrigado por seu pedido! Nós estamos processando!";
+                dispatcher.send("ECOMMERCE_ENVIAR_EMAIL", key, email);
             }
-            System.out.println("sucesso enviando " + data.topic() +
-                    ":::partition " + data.partition() +
-                    "/ offset " + data.offset() +
-                    "/ timestamp " + data.timestamp());
-        };
-
-        producer.send(record, callback).get();
-        producer.send(emailRecord, callback).get();
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+        }
     }
 }
